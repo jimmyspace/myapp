2da6dca53f504b627d8f51886c7f90d8f02760f7
Jimmy <791092326@qq.com>
2018-01-06 15:27:59 +0800

    跨域

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 
-app.all('*', function(req, res, next) {
+app.use('*', function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "X-Requested-With");
     res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
