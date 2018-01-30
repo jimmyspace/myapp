44d8299711ee9574dd70e5fc88bf91c9fc2be0b6
Jimmy <791092326@qq.com>
2018-01-30 13:40:25 +0800

    修正数据库配置

--- db/db_config.js
+++ db/db_config.js
@@ -1,5 +1,4 @@
- console.log(process.env.NODE_ENV);
- const develop_config = {
+const develop_config = {
    host: '127.0.0.1',
    user: 'root',
    password: 'root',
@@ -7,15 +6,4 @@
    port: 3306,
    multipleStatements: true //可以执行多条语句
  }
- const product_config = {
-   //host: '98.142.142.208',
-   host: '127.0.0.1',
-   user: 'root',
-   password: 'root',
-   database: 'test',
-   port: 3306,
-   multipleStatements: true //可以执行多条语句
- }
- module.exports = {
-   mysql: process.env.NODE_ENV === 'develop' ? develop_config : product_config
- };
+ module.exports = develop_config;
